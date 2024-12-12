a41d43faf506887e600d209f629dc8eaf0a97c3a
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-12-12 17:40:14 +0800

    pip3 install all

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,11 @@
-pip install matplotlib scikit-learn wandb opencv-python h5py
+pip3 install matplotlib scikit-learn wandb opencv-python h5py
 apt-get update
 apt-get -y install libgl1-mesa-glx -y
 apt-get -y install libglib2.0-0 -y
-pip install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
-pip install jupyter notebook
+pip3 install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
+pip3 install jupyter notebook
 pip3 install --upgrade Cython
-pip install kornia
+pip3 install kornia
 
 
 
