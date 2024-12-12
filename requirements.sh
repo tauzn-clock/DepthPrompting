be567439a7920bebd428380d142018ad13af9d9c
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-12-12 17:23:52 +0800

    include docker

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,7 @@ apt-get -y install libgl1-mesa-glx -y
 apt-get -y install libglib2.0-0 -y
 pip install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
 pip install jupyter notebook
-pip install kornia==0.5.5
+pip install kornia
 
 
 
