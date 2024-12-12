9dd01414d025282e540ebd5dca035e704c639eeb
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-12-12 17:48:02 +0800

    put in docker directly

--- requirements.sh
+++ requirements.sh
@@ -6,11 +6,10 @@ pip3 install mmcv-full==1.3.13 -f https://download.openmmlab.com/mmcv/dist/cu111
 pip3 install jupyter notebook
 pip3 install --upgrade Cython
 pip3 install kornia
-
-
-
-
-
-
-
-
+pip3 install torch torchvision
+pip3 install h5py tqdm
+pip3 install scipy
+pip3 install matplotlib
+pip3 install nuscenes-devkit
+pip3 install imageio
+pip3 install pillow
