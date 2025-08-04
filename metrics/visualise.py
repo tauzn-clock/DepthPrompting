c3c6b64ab80a777a4dba9518c39cb84a49e732f6
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-08-04 10:03:20 +0000

    include ours

--- metrics/visualise.py
+++ metrics/visualise.py
@@ -20,18 +20,20 @@ def add_new_plot(ax, x, y, prop,label):
     ax.plot(x,y,prop,label=label)
     return ax
 
-def plot_all(name, ylabel, i):
+def plot_all(name, ylabel, i, ylimit):
     fig, ax = plt.subplots(1,1,)
     ax = add_new_plot(ax, prop[:,0], prop[:,i], 'k--', 'Best Fit')
     ax = add_new_plot(ax, depthprompting[:,0], depthprompting[:,i],'b--', 'DepthPrompting')
+    ax = add_new_plot(ax, our[:,0], our[:,i],'r--', 'Compressed Sensing')
     ax.set_xscale('log')
     ax.grid(True)
     ax.minorticks_on()
     ax.grid(True, which='major', linestyle='-', linewidth=0.8, color='gray')
-    ax.grid(True, which='minor', linestyle='--', linewidth=0.4, color='gray')
+    ax.grid(True, which='minor', linestyle='--', linewidth=0.4, color='lightgray')
     ax.legend()
     ax.set_xlabel("Sampled Points Percentage")
     ax.set_ylabel(ylabel)
+    ax.set_ylim(ylimit[0],ylimit[1])
     
     fig.savefig(name, bbox_inches="tight", pad_inches=0)
 
@@ -39,7 +41,8 @@ MAX_PT = 224*302
 
 depthprompting = read_csv("/DepthPrompting/metrics/DepthPromptingNYU.csv", MAX_PT)
 prop = read_csv("/DepthPrompting/metrics/PropNYU.csv", MAX_PT)
+our = read_csv("/DepthPrompting/metrics/OursNYU.csv", MAX_PT)
 
-plot_all("RMSE_depth_prompting.png", "RMSE/m",1)
-plot_all("MAE_depth_prompting.png", "MAE/m",2)
-plot_all("DELTA1_depth_prompting.png", "DELTA1",3)
+plot_all("RMSE_depth_prompting.png", "RMSE/m",1,[0,0.35])
+plot_all("MAE_depth_prompting.png", "MAE/m",2,[0,0.25])
+plot_all("DELTA1_depth_prompting.png", "DELTA1",3,[0.92,1])
