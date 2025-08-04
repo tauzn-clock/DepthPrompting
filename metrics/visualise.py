c6bfaf9cf245ac09e4aa8691a8357990dc8472a3
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-08-04 08:49:19 +0000

    clean up metric

---
+++ metrics/visualise.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+def read_csv(csv_path, MAX_PT):
+    with open(csv_path, "r") as test:
+        
+        
+read_csv("/DepthPrompting/metrics/DepthPromptingNYU.csv", 224*302)
