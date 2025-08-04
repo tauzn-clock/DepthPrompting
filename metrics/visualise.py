f56ab161ac4d5ca65ee36733d8c73a28d3da21c6
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-08-04 09:39:39 +0000

    vis better

--- metrics/visualise.py
+++ metrics/visualise.py
@@ -16,17 +16,30 @@ def read_csv(csv_path, MAX_PT):
     
     return arr
     
-def add_new_plot(ax, x, y, prop):
-    ax.plot(x,y,prop)
+def add_new_plot(ax, x, y, prop,label):
+    ax.plot(x,y,prop,label=label)
     return ax
+
+def plot_all(name, ylabel, i):
+    fig, ax = plt.subplots(1,1,)
+    ax = add_new_plot(ax, prop[:,0], prop[:,i], 'k--', 'Best Fit')
+    ax = add_new_plot(ax, depthprompting[:,0], depthprompting[:,i],'b--', 'DepthPrompting')
+    ax.set_xscale('log')
+    ax.grid(True)
+    ax.minorticks_on()
+    ax.grid(True, which='major', linestyle='-', linewidth=0.8, color='gray')
+    ax.grid(True, which='minor', linestyle='--', linewidth=0.4, color='gray')
+    ax.legend()
+    ax.set_xlabel("Sampled Points Percentage")
+    ax.set_ylabel(ylabel)
+    
+    fig.savefig(name, bbox_inches="tight", pad_inches=0)
 
 MAX_PT = 224*302
 
 depthprompting = read_csv("/DepthPrompting/metrics/DepthPromptingNYU.csv", MAX_PT)
 prop = read_csv("/DepthPrompting/metrics/PropNYU.csv", MAX_PT)
 
-fig, ax = plt.subplots(1,1,)
-ax = add_new_plot(ax, prop[:-1,0], prop[:-1,1], 'r--')
-ax = add_new_plot(ax, depthprompting[:-1,0], depthprompting[:-1,1],'b--')
-
-fig.savefig("test.png")
+plot_all("RMSE_depth_prompting.png", "RMSE/m",1)
+plot_all("MAE_depth_prompting.png", "MAE/m",2)
+plot_all("DELTA1_depth_prompting.png", "DELTA1",3)
